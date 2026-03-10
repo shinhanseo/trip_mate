8f95b12593d5085e6d940b31c39ca4d5fe0e160e
shinhanseo <imkara1@naver.com>
2026-03-11 01:20:27 +0900

    Chore : 패키지 명 변경

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.frontend"
+    namespace = "com.hanseo.mohaeng"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
